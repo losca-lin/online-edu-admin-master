ea711d1c6514ef5def3b92e4439e812d60b9b98e
losca <783840358@qq.com>
2023-05-23 00:52:49 +0800

    初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '在线家教平台管理员端',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+
+}
